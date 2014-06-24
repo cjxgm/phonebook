e66b70918a06ebf6e5ac17e7d547761cc83bc923
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-06-24 21:18:32 +0800

    added license info

--- src/nde/action/edit.cc
+++ src/nde/action/edit.cc
@@ -1,3 +1,7 @@
+// vim: noet ts=4 sw=4 sts=0 fenc=utf8
+// Licensed under the MIT License. See COPYING for more details.
+// (C) 2014. eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
+
 #include "edit.hh"
 
 namespace nde
